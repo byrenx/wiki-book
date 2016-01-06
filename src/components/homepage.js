ff3b1b729626421a011d898f0aa46ef6d9def055
byrenx <jofel.bayron@cloudsherpas.com>
2016-01-06 20:52:10 +0800

    add component

---
+++ src/components/homepage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function(){
+		return (
+			<div className="jumbotron">
+				<h1>Administration</h1>
+				<p>This page is under development.</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Home;
